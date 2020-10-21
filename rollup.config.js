f4434eb9e5b66f83dcc04c6bdaabd2ac46559308
Peter Hughes <12073314+phughesmcr@users.noreply.github.com>
2020-10-21 12:04:52 +0100

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,49 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import pkg from './package.json';
+
+const extensions = [
+  '.js', '.jsx', '.ts', '.tsx',
+];
+
+const name = pkg.name;
+
+export default {
+  input: './src/index.ts',
+
+  // https://rollupjs.org/guide/en#external-e-external
+  external: [],
+
+  plugins: [
+    // Allows node_modules resolution
+    resolve({ extensions }),
+
+    // Allow bundling cjs modules. Rollup doesn't understand cjs
+    commonjs(),
+
+    // Compile TypeScript/JavaScript files
+    babel({
+      extensions,
+      babelHelpers: 'bundled',
+      include: ["src/**/*"],
+      exclude: ["node_modules/**"],
+    }),
+  ],
+
+  output: [{
+    file: pkg.main,
+    format: 'umd',
+    name,
+  }, {
+    file: pkg.module,
+    format: 'es',
+  }, {
+    file: pkg.browser,
+    format: 'iife',
+    name,
+
+    // https://rollupjs.org/guide/en#output-globals-g-globals
+    globals: {},
+  }],
+};
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+// placeholder file to avoid "No inputs were found in config file." Typescript error
+export const test = Promise.allSettled([console.log(1)]);
